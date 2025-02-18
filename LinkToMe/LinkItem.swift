65a040b37fb55ae7d93944018b0a10db04f3d980
GwanSon <id147369@naver.com>
2025-02-18 17:14:22 +0900

    feat: 링크 데이터 생성

---
+++ LinkToMe/LinkItem.swift
@@ -0,0 +1,37 @@
+//
+//  LinkItem.swift
+//  LinkToMe
+//
+//  Created by 심관혁 on 2/18/25.
+//
+
+import SwiftData
+import Foundation
+
+@Model
+final class LinkItem {
+    @Attribute(.unique) var id: UUID
+    var url: URL
+    var title: String
+    var createdDate: Date
+    var previewImage: Data?
+    @Relationship var tags: [Tag]
+    
+    init(url: URL, title: String, tags: [Tag] = []) {
+        self.id = UUID()
+        self.url = url
+        self.title = title
+        self.createdDate = Date()
+        self.tags = tags
+    }
+}
+
+@Model
+final class Tag {
+    @Attribute(.unique) var name: String
+    @Relationship var links: [LinkItem] = []
+    
+    init(name: String, links: [LinkItem] = []) {
+        self.name = name
+    }
+}

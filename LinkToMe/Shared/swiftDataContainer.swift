edf1c81b7a54ada8ef625819797f6012d3c4fa56
GwanSon <id147369@naver.com>
2025-03-26 15:06:13 +0900

    feat: SwiftData Container 생성

---
+++ LinkToMe/Shared/swiftDataContainer.swift
@@ -0,0 +1,33 @@
+//
+//  swiftDataContainer.swift
+//  LinkToMe
+//
+//  Created by 심관혁 on 3/23/25.
+//
+
+import SwiftUI
+import SwiftData
+
+func createSharedModelContainer() -> ModelContainer {
+    let schema = Schema([LinkItem.self])
+    
+    let groupID = "group.kr.gwanson.LinkToMe"
+    
+    guard let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupID) else {
+        fatalError("App Group 설정에 문제가 있습니다.")
+    }
+    
+    let sharedURL = url.appendingPathComponent("shared.sqlite")
+    
+    let configuration = ModelConfiguration(
+        schema: schema,
+        url: sharedURL,
+        cloudKitDatabase: .none
+    )
+    
+    do {
+        return try ModelContainer(for: schema, configurations: [configuration])
+    } catch {
+        fatalError("SwiftData 컨테이너를 생성할 수 없습니다: \(error)")
+    }
+}

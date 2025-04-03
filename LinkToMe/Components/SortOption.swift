9e576265b39b7a4105e6dd55895f9b63aac491ef
GwanSon <id147369@naver.com>
2025-04-03 15:46:29 +0900

    feat: 정렬 기준 추가

---
+++ LinkToMe/Components/SortOption.swift
@@ -0,0 +1,15 @@
+//
+//  SortOption.swift
+//  LinkToMe
+//
+//  Created by 심관혁 on 4/2/25.
+//
+
+import Foundation
+
+enum SortOption {
+    case dateNewest
+    case dateOldest
+    case titleAtoZ
+    case titleZtoA
+}

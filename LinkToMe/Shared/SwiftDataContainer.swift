015c2390f6f9b12485589d821d6e7fe4ab4164d6
GwanSon <id147369@naver.com>
2025-04-11 15:29:24 +0900

    feat: 데이터 컨테이너에 카테고리 추가

--- LinkToMe/Shared/SwiftDataContainer.swift
+++ LinkToMe/Shared/SwiftDataContainer.swift
@@ -8,26 +8,36 @@
 import SwiftUI
 import SwiftData
 
+class SharedModelContainer {
+    static let shared = SharedModelContainer()
+    
+    let container: ModelContainer
+    
+    private init() {
+        let schema = Schema([LinkItem.self, CategoryItem.self])
+        
+        let groupID = "group.kr.gwanson.LinkToMe"
+        
+        guard let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupID) else {
+            fatalError("App Group 설정에 문제가 있습니다.")
+        }
+        
+        let sharedURL = url.appendingPathComponent("shared.sqlite")
+        
+        let configuration = ModelConfiguration(
+            schema: schema,
+            url: sharedURL,
+            cloudKitDatabase: .none
+        )
+        
+        do {
+            container = try ModelContainer(for: schema, configurations: [configuration])
+        } catch {
+            fatalError("SwiftData 컨테이너를 생성할 수 없습니다: \(error)")
+        }
+    }
+}
+
 func createSharedModelContainer() -> ModelContainer {
-    let schema = Schema([LinkItem.self])
-    
-    let groupID = "group.kr.gwanson.LinkToMe"
-    
-    guard let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupID) else {
-        fatalError("App Group 설정에 문제가 있습니다.")
-    }
-    
-    let sharedURL = url.appendingPathComponent("shared.sqlite")
-    
-    let configuration = ModelConfiguration(
-        schema: schema,
-        url: sharedURL,
-        cloudKitDatabase: .none
-    )
-    
-    do {
-        return try ModelContainer(for: schema, configurations: [configuration])
-    } catch {
-        fatalError("SwiftData 컨테이너를 생성할 수 없습니다: \(error)")
-    }
+    return SharedModelContainer.shared.container
 }

71633a09e72b9e0f8db1c0f203f4059c81b06260
GwanSon <id147369@naver.com>
2025-04-11 15:27:52 +0900

    feat: 데이터 저장 모델 수정

--- LinkToMe/Components/LinkItem.swift
+++ LinkToMe/Components/LinkItem.swift
@@ -9,16 +9,42 @@ import SwiftData
 import Foundation
 
 @Model
+class CategoryItem {
+    var id: UUID
+    var name: String
+    var icon: String
+    var parentCategory: CategoryItem?
+    
+    @Relationship(deleteRule: .cascade, inverse: \CategoryItem.parentCategory)
+    var subCategories: [CategoryItem]?
+    
+    @Relationship(deleteRule: .nullify, inverse: \LinkItem.category)
+    var links: [LinkItem]?
+    
+    init(name: String, icon: String, parentCategory: CategoryItem? = nil) {
+        self.id = UUID()
+        self.name = name
+        self.icon = icon
+        self.parentCategory = parentCategory
+        self.subCategories = []
+        self.links = []
+    }
+}
+
+@Model
 final class LinkItem {
-    @Attribute(.externalStorage)
+    var id: UUID
     var url: String
     var title: String
     var personalMemo: String?
     var savedDate: Date
-    init(url: String, title: String, personalMemo: String? = nil) {
+    var category: CategoryItem?
+    init(url: String, title: String, personalMemo: String? = nil, category: CategoryItem? = nil) {
+        self.id = UUID()
         self.url = url
         self.title = title
         self.personalMemo = personalMemo
         self.savedDate = Date()
+        self.category = category
     }
 }
